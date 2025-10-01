3ec87b51d2eddec77018db993941724d140a75b6
wxnnvs <136503688+wxnnvs@users.noreply.github.com>
2025-10-01 08:45:20 +0000

    change dns url

--- protocol/gurtca/src/main.rs
+++ protocol/gurtca/src/main.rs
@@ -12,7 +12,7 @@ struct Cli {
     #[command(subcommand)]
     command: Commands,
     
-    #[arg(long, default_value = "gurt://dns.web")]
+    #[arg(long, default_value = "gurt://49.12.6.233:4878")]
     ca_url: String,
 }
 
